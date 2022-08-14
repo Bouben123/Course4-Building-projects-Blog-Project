5e8ac3b21624df8a8278fb0cab887c722ea14ab6
Bouben123 <jeemin998@gmail.com>
2022-08-14 18:22:57 +0200

    apload all blog project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Article from './Article';
 
+const articles = [
+  {
+    title: "You are more pretty than you thought",
+    content: "our article is here ,come and visit",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp5-uluQo4Fwrd1vgCeM95bdIfZ2aXd0ddgdfFxGjB&s"
+  },
+  {
+    title: "Be happy even the world go against you",
+    content: "our article is here ,come and visit",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp5-uluQo4Fwrd1vgCeM95bdIfZ2aXd0ddgdfFxGjB&s"
+  },
+  {
+    title: "You are who you are, don't change for anyone",
+    content: "our article is here ,come and visit",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp5-uluQo4Fwrd1vgCeM95bdIfZ2aXd0ddgdfFxGjB&s"
+  }
+]
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {articles.map(article =>(
+          <Article
+            title={article.title}
+            content={article.content}
+            image={article.image}
+          />
+        ))}
     </div>
   );
 }
